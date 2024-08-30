6433aedf91d8fb19a509c0036feb6dfa0a375abc
Vast-Academy <syncvap@gmail.com>
2024-08-30 19:35:56 +0530

    dynamically add header footer

--- js/app.js
+++ js/app.js
@@ -1,26 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navItems = `
-        <li><a href="../">Home</a></li>
-        <li><a href="../projects/">Projects</a></li>
-        <li class="dropdown"><a href="#">Services</a>
-            <ul>
-                <li><a href="../web-development/">Web Development</a></li>
-                <li><a href="../software-development/">Software Development</a></li>
-                <li><a href="../digital-marketing/">Digital Marketing</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="../contact-us/">Get in Touch</a></li>
-        <li><a href="../faq/">FAQ</a></li>
-    `;
+// Navbar items
+const navbarItems = `
+    <li><a href="../">Home</a></li>
+    <li><a href="../projects/">Projects</a></li>
+    <li class="dropdown"><a href="#">Services</a>
+        <ul>
+            <li><a href="../web-development/">Web Development</a></li>
+            <li><a href="../software-development/">Software Development</a></li>
+            <li><a href="../digital-marketing/">Digital Marketing</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Pricing</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="../contact-us/">Get in Touch</a></li>
+    <li><a href="../faq/">FAQ</a></li>
+`;
 
-    // Add navItems to desktop menu, mobile menu, and sticky header menu
-    const navigationMenus = document.querySelectorAll('.navigation.clearfix');
-    navigationMenus.forEach(menu => {
-        menu.innerHTML = navItems;
-    });
-});
+// Top-left items (Email and Address)
+const topLeftContent = `
+    <ul class="list-style-one">
+        <li><i class="fa fa-envelope"></i> <a href="mailto:info@vacomputers.com">info@vacomputers.com</a></li>
+        <li><i class="fa fa-map-marker"></i> <a href="https://maps.app.goo.gl/dAuzu5yLwEQqYbMb9">VA Computers, New Bus Stand to Railway station (Majitha-143601) Distt. Amritsar</a></li>
+    </ul>
+`;
+
+// Top-right items (Social Links)
+const topRightContent = `
+    <ul class="social-icon-one">
+        <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+        <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+        <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+    </ul>
+`;
+
+// Contact list in the mobile menu
+const contactListContent = `
+    <li>
+        <div class="contact-info-box">
+            <i class="icon lnr-icon-phone-handset"></i>
+            <span class="title">Call Now</span>
+            <a href="tel:+9356393094">+91 93563-93094</a>
+        </div>
+    </li>
+    <li>
+        <div class="contact-info-box">
+            <span class="icon lnr-icon-envelope1"></span>
+            <span class="title">Send Email</span>
+            <a href="mailto:info@vacomputers.com">info@vacomputers.com</a>
+        </div>
+    </li>
+    <li>
+        <div class="contact-info-box">
+            <span class="icon lnr-icon-clock"></span>
+            <span class="title">Opening Hours</span>
+            Mon - Sat 9:00 A.M to 7:00 P.M <br> Sunday - CLOSED
+        </div>
+    </li>
+`;
+
+// Social links in the mobile menu
+const socialLinksContent = `
+    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+`;
+
+// Injecting content into the HTML
+document.getElementById('navigation').innerHTML = navbarItems;
+document.getElementById('top-left').innerHTML = topLeftContent;
+document.getElementById('top-right').innerHTML = topRightContent;
+document.getElementById('contact-list').innerHTML = contactListContent;
+document.getElementById('social-links').innerHTML = socialLinksContent;
 
 
 
